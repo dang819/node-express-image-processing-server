3ee6b038ac6316948a3bb1f849f0b72ba3e92a03
Damian Nunez <damiannunez@Damians-MacBook-Pro.local>
2021-04-15 20:47:16 -0600

    module-2 Storing the image on disk

---
+++ api/src/router.js
@@ -0,0 +1,28 @@
+const { Router } = require('express');
+const multer = require('multer');
+const storage = multer.diskStorage({destination: 'api/uploads/', filename: filename});
+const upload = multer({fileFilter: fileFilter, storage: storage});
+
+const router = Router();
+
+function filename(req, file, cb) {
+    cb(null, file.originalname);
+}
+
+function fileFilter(req, file, cb) {
+    if(file.mimetype !== 'image/png') {
+        req.fileValidationError = 'Wrong file type';
+        cb(null, false, new Error('Wrong file type'))
+    } else {
+        cb(null, true);
+    }
+}
+
+router.post('/upload', upload.single('photo'), (req, res)=> {
+    if(req.fileValidationError){
+        return res.status(400).json({error: req.fileValidationError});
+    }
+    return res.status(201).json({success: true});
+});
+
+module.exports = router;
