38b766f54d4e235dbe2b44f4b39cf17af01e5d5a
Damian Nunez <damiannunez@Damians-MacBook-Pro.local>
2021-04-15 22:16:36 -0600

    module-4 Creating the parent thread

--- api/src/router.js
+++ api/src/router.js
@@ -1,14 +1,16 @@
 const { Router } = require('express');
 const multer = require('multer');
 const photoPath = require('path').resolve(__dirname, '../../client/photo-viewer.html');
-const storage = multer.diskStorage({destination: 'api/uploads/', filename: filename});
-const upload = multer({fileFilter: fileFilter, storage: storage});
+const imageProcessor = require('./imageProcessor');
 
 const router = Router();
 
 function filename(req, file, cb) {
     cb(null, file.originalname);
 }
+
+const storage = multer.diskStorage({destination: 'api/uploads/', filename: filename});
+const upload = multer({fileFilter: fileFilter, storage: storage});
 
 function fileFilter(req, file, cb) {
     if(file.mimetype !== 'image/png') {
@@ -19,9 +21,14 @@ function fileFilter(req, file, cb) {
     }
 }
 
-router.post('/upload', upload.single('photo'), (req, res)=> {
+router.post('/upload', upload.single('photo'), async (req, res)=> {
     if(req.fileValidationError){
         return res.status(400).json({error: req.fileValidationError});
+    }
+    try {
+        await imageProcessor(req.file.filename);
+    } catch (error) {
+        
     }
     return res.status(201).json({success: true});
 });
